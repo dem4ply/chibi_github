56d4ba1154cb52f3aabb624f208fc9b656237cb6
dem4ply <dem4ply@gmail.com>
2025-09-20 16:10:51 -0600

    Bump version: 0.1.2 → 0.2.0

--- chibi_github/__init__.py
+++ chibi_github/__init__.py
@@ -5,4 +5,4 @@ from chibi_github.chibi_github import Github_api
 __all__ = [ 'Github_api' ]
 __author__ = """dem4ply"""
 __email__ = 'dem4ply@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.2.0'
